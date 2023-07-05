e2198bc89ea2a73530558da8775f5634e735b351
SoulOuatt <104140120+SoulOuatt@users.noreply.github.com>
2023-07-05 12:13:36 +0000

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-#API_URL = "https://eth-mainnet.alchemyapi.io/v2/YAwsPVWGGRUF4aTsCTCYdgTELH4aTEPr" 
-TESTNET_URL = "https://eth-goerli.g.alchemy.com/v2/smCLEzFgtbFjnfgPNNokyGL95CJqG_GK"
-PRIVATE_KEY = "93a0b36184449ed294add0d3619ff0a8f42f904b5260520f72ca7ccd7ccaf317"
-WALLET_ADDRESS = "0xe3242ca2b4036f90f42C6D7861af28d06c6161cC"
